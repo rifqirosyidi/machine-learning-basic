ea418857b1d07504b278cd7bc07880a7713d0aa7
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-11-03 22:57:14 +0700

    Features and Labels

--- 01. Linier Regression/main.py
+++ 01. Linier Regression/main.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas as pd
 import quandl
 
@@ -13,6 +15,16 @@ data_frame['HL_PCT'] = (data_frame['Adj. High'] - data_frame['Adj. Close']) / da
 data_frame['PCT_CHANGE'] = (data_frame['Adj. Close'] - data_frame['Adj. Open']) / data_frame['Adj. Open'] * 100.0
 
 # Get The Data That only we wanted
-data_frame = data_frame[['Adj. Close', 'HL_PCT', 'PCT_CHANGE', 'Adj. Volume']]
+data_frame = data_frame[['Adj. Close', 'HL_PCT', 'PCT_CHANGE', 'Adj. Volume']]  # THIS IS FEATURES
+
+# Data to be Forecasted
+forecast_col = 'Adj. Close'  # Stock End Price
+data_frame.fillna(-99999, inplace=True)
+
+# Get The last 1 % of the all data to predict the next stock price
+forecast_out = int(math.ceil(0.01*len(data_frame)))
+
+# Shift the column to 1 of the data %
+data_frame['label'] = data_frame[forecast_col].shift(-forecast_out)
 
 print(data_frame.head())
